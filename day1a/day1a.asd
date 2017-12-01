dcb1bd8c4d2bae47de88f348b5962d5cedc23188
sergio garcia <sergio@crazyrobot.net>
2017-12-01 08:18:17 -0500

    Style

--- day1a/day1a.asd
+++ day1a/day1a.asd
@@ -1,9 +1,9 @@
 ;;;; day1a.asd
 
 (asdf:defsystem #:day1a
-  :description "Describe day1a here"
-  :author "Your Name <your.name@example.com>"
-  :license "Specify license here"
+  :description "day1a"
+  :author "Sergio Garcia <sergio.garcia@gmail.com>"
+  :license "artistic license"
   :depends-on (#:alexandria)
   :serial t
   :components ((:file "package")
--- day1a/day1a.lisp
+++ day1a/day1a.lisp
@@ -5,14 +5,18 @@
 	 (read-line stream)))
 
 (defun calculate-sum (input)
-  (labels ((char-to-int (char)
+  (if input
+      (labels ((char-to-int (char)
 	     (- (char-int char) 48))
 	   (next-index (index length)
 	     (if (= (1- length) index)
 		 0
 		 (1+ index))))
-    (loop
-       with length = (length input)
-       for i below length
-       for j = (next-index i length) then (next-index i length) 
-       when (char= (aref input i) (aref input j)) sum (char-to-int (aref input i)))))
+	(loop
+	   with length = (length input)
+	   for i below length
+	   for j = (next-index i length) then (next-index i length) 
+	   when (char= (aref input i) (aref input j)) sum (char-to-int (aref input i))))
+      0))
+
+
